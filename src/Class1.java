4add7d93e424e092ed3e7557d8ea4c8907b95f6b
NinaRuiz <nina.ruizandres@gmail.com>
2020-03-14 13:19:23 +0100

    Add variables.

--- src/Class1.java
+++ src/Class1.java
@@ -1,9 +1,13 @@
+import controller.GameController;
+
 public class Class1 {
     public static void main (String[]args) {
         /** Historieta: Elige tu camino
          * 1r Decidir las diferentes ramas y elegir una historia
          * 2n
          */
+        GameController gamecontroller = new GameController();
+        gamecontroller.init();
     }
 }
 
