4c6d973e34274840987bd815b9b9150f9aeee377
NinaRuiz <nina.ruizandres@gmail.com>
2020-03-14 13:02:17 +0100

    First commit

--- src/Class1.java
+++ src/Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
     public static void main (String[]args) {
-        /**Historieta: Elige tu camino
+        /** Historieta: Elige tu camino
          * 1r Decidir las diferentes ramas y elegir una historia
          * 2n
          */
