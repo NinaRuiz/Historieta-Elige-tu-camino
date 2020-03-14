a2f11827359901c4b09559a6cbf484e6602fc77a
NinaRuiz <nina.ruizandres@gmail.com>
2020-03-14 13:28:25 +0100

    I don't know what we actually made on this.

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -2,7 +2,9 @@ package controller;
 
 import model.Node;
 
+import java.sql.SQLOutput;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GameController {
     public static final int firstStep = 0;
@@ -17,6 +19,12 @@ public class GameController {
      * @param node
      */
     public void showNode (Node node) {
-
+        System.out.println(node.getMessage());
+        System.out.println(node.getQuestion());
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        int option = Integer.parseInt(line);
+        Node nodeChild = this.nodes.get(option);
+        this.showNode(nodeChild);
     }
 }
