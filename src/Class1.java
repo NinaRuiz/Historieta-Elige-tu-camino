1753154ee0417f6e5a082c5b37615c8a5251ecd0
NinaRuiz <nina.ruizandres@gmail.com>
2020-03-14 13:00:52 +0100

    Add notes

--- src/Class1.java
+++ src/Class1.java
@@ -1,2 +1,9 @@
 public class Class1 {
+    public static void main (String[]args) {
+        /**Historieta: Elige tu camino
+         * 1r Decidir las diferentes ramas y elegir una historia
+         * 2n
+         */
+    }
 }
+
