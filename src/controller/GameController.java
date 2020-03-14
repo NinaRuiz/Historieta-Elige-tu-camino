e7704872fd606dd083985ceae79f05a08eff7371
NinaRuiz <nina.ruizandres@gmail.com>
2020-03-14 13:17:12 +0100

    Create methods and class.

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import model.Node;
+
+import java.util.Map;
+
 public class GameController {
+    public static final int firstStep = 0;
+    Map<Integer, Node> nodes;
+
+    public void init () {
+
+    }
+
+    /**
+     *
+     * @param node
+     */
+    public void showNode (Node node) {
+
+    }
 }
